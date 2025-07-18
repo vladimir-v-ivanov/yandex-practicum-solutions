16c5500bf29d6724f5047b4968545918b0ba4db9
Vladimir Ivanov <vladimir@ivanov.click>
2025-07-18 22:30:14 +0400

    Initial commit

--- sprint7/a.cpp
+++ sprint7/a.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    // Проходим по массиву циклом
-    // На каждой итерации у нас либо есть купленная акция, либо нет
-    // Если есть купленная акция то проверяем цену за два последних дня, если второй день дешевле чем первый, то продаем по цене первого
+    int days_num;
+    cin >> days_num;
+
+    if (days_num < 2) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    vector<int> prices(days_num);
+    for (int i = 0; i < days_num; i++) {
+        cin >> prices[i];
+    }
+
+    int bought = 0;
+    int total  = 0;
+
+    for (int i = 1; i < days_num; i++) {
+        if (prices[bought] < prices[i]) {
+            total += prices[i] - prices[bought];
+            bought = i;
+        } else {
+            bought = i;
+        }
+    }
+
+    cout << total << endl;
 }
--- sprint7/b.cpp
+++ sprint7/b.cpp
@@ -1,5 +1,57 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int str_to_int(string str) {
+    const size_t len = str.length();
+
+    if (const size_t pos = str.find('.'); pos != string::npos) {
+        str.insert(len, 2 - (len - pos - 1), '0');
+        str.erase(pos, 1);
+    }
+    else {
+        str.insert(len, 2, '0');
+    }
+
+    return atoi(str.c_str());
+}
 
 int main() {
+    int classes_num;
+    cin >> classes_num;
 
+    vector<pair<string, string>> schedule;
+    schedule.reserve(classes_num);
+
+    for (int i = 0; i < classes_num; i++) {
+        string start, end;
+        cin >> start >> end;
+
+        schedule.emplace_back(start, end);
+    }
+
+    ranges::sort(schedule, [](const auto& a, const auto& b) {
+        return a.second == b.second ? str_to_int(a.first) < str_to_int(b.first) : str_to_int(a.second) < str_to_int(b.second);
+    });
+
+    int                          last_end = -1;
+    int                          total    = 0;
+    vector<pair<string, string>> result;
+
+    for (const auto& [start, end] : schedule) {
+        if (last_end <= str_to_int(start)) {
+            total++;
+            result.emplace_back(start, end);
+            last_end = str_to_int(end);
+        }
+    }
+
+    cout << total << endl;
+
+    for (const auto& [start, end] : result) {
+        cout << start << " " << end << endl;
+    }
 }
